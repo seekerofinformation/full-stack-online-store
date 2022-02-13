4624c206d111d4fec058f07a89e26570e9c10be0
roman.timonin <roman,timonin@solbeg.com>
2022-02-13 13:06:25 +0300

    Added env file, added models, DB configuration, dependencies

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require("cors")
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT,() => {})
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start();
